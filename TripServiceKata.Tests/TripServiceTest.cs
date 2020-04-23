20eafca6145527e056ff697d120d0398f40e42c2
Ian Letourneau <letourneau.ian@gmail.com>
2020-04-23 13:23:49 -0400

    Setup dotnet CLI

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,16 @@
-﻿namespace TripServiceKata.Tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
 {
+    [TestClass]
     public class TripServiceTest
     {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TripService tripService = new TripService();
+        }
     }
 }
